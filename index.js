7cd09bc653ceaa53947a46d7699ed19df8ba66ec
Maison Armani <masonarmani38@gmail.com>
2021-11-04 08:24:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,4 +11,16 @@ export default class Tester{
             });
         });
     }
+    
+    async getAlbumByUserId (userId) {
+        const requestUrl = `https://jsonplaceholder.typicode.com/albums/${userId}/photos?_limit=5`;
+        return new Promise((resolve, reject) => {
+            axios.get(requestUrl, (err, res, body) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(JSON.parse(body));
+            });
+        });
+    }
 }
